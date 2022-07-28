1b6680d75da9af8f3f3594d0572a9ce28f00e90a
griffin <35904717+gjbsaiia@users.noreply.github.com>
2022-07-28 18:53:25 -0400

    added error handling

--- youtubeToMp3.py
+++ youtubeToMp3.py
@@ -9,57 +9,90 @@ class ProgressLogger(object):
     def __init__(self, debugMode = False):
         self.debugMode = debugMode
     def debug(self, msg):
-        if not self.debugMode:
-            pass
-        else:
-            print(msg)
+        print(msg)
     def warning(self, msg):
         if not self.debugMode:
             pass
         else:
-            print(msg)
+            print("warning: " + msg)
     def error(self, msg):
+        updateSettingsFile()
         print(msg)
+    
+playListCount: int = 0
+settingsFile: string = None
 
 def main(args: argparse.Namespace):
+    global playListCount, settingsFile
+    playListCount = 0
     urls = []
-    if(os.path.isfile(args.config)):
+    if(args.config and os.path.isfile(args.config)):
         url = getOptionsFromFile(args.config)
+        settingsFile = args.config
     else:
         url = input("Input youtube link: ")
     urls.append(url)
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download(urls)
 
+def updateSettingsFile():
+    global playListCount, settingsFile
+    if(playListCount == 0):
+        return
+    if(not settingsFile):
+        print("job failed to complete.\r\nrelaunch playlist link with option 'playliststart' set to "+playListCount+".")
+        return
+    print("updating settings file: "+settingsFile)
+    lines = []
+    with open(settingsFile, 'r', encoding=sys.getfilesystemencoding()) as argFile:
+        lines = argFile.readlines()
+    rewrite = []
+    for line in lines:
+        i = line.find('playliststart')
+        if(i >= 0):
+            i += len('playliststart')+1
+            end_i = line.find(",", i)
+            arg = (line[i : end_i] if end_i > 0 else line[i:]).strip()
+            rewrite.append('playliststart: '+str(playListCount + int(arg) if arg.isdigit() else playListCount)+",")
+        else:
+            rewrite.append(line)
+    with open(settingsFile, "w", encoding=sys.getfilesystemencoding()) as argFile:
+        argFile.writelines(rewrite)
+
 def check_complete(d):
+    global playListCount
     if d['status'] == 'finished':
         print('Done downloading, now converting ...')
+        playListCount += 1
 
 def getOptionsFromFile(path: string):
     args = []
     with open(path, 'r', encoding=sys.getfilesystemencoding()) as argFile:
         args = argFile.readlines()
-    options = { 'playlistStart': None, 'playlistEnd': None, 'url': None }
+    options = { 'playliststart': None, 'playlistend': None, 'url': None, 'sleep_interval': None }
     for line in args:
-        line = line.replace(" ", "")
         for option in options.keys():
             i = line.find(option)
             if(i >= 0):
                 i += len(option)+1
-                end_i = int(line.find(",", i))
-                arg = line[i : end_i] if end_i > 0 else line[i:]
+                end_i = line.find(",", i)
+                arg = (line[i : end_i] if end_i > 0 else line[i:]).strip()
                 options[option] = arg
-    
-    if(options["playlistStart"]):
+    if(options["playliststart"]):
         try:
-            ydl_opts.update({'playlistStart', int(options["playlistStart"])})
+            ydl_opts.update({'playliststart': int(options["playliststart"])})
         except ValueError:
             print("playlist start not a valid number")
-    if(options["playlistEnd"]):
+    if(options["playlistend"]):
         try:
-            ydl_opts.update({'playlistEnd', int(options["playlistEnd"])})
+            ydl_opts.update({'playlistend': int(options["playlistend"])})
         except ValueError:
             print("playlist end not a valid number")
+    if(options["sleep_interval"]):
+        try:
+            ydl_opts.update({'sleep_interval': int(options["sleep_interval"])})
+        except ValueError:
+            print("sleep_interval not a valid number")
 
     return options["url"]
 
@@ -71,7 +104,7 @@ ydl_opts = {
         'preferredcodec': 'mp3',
         'preferredquality': '192'
     }],
-    'logger': ProgressLogger(True),
+    'logger': ProgressLogger(False),
     'progress_hooks': [check_complete]
 }
 
@@ -85,6 +118,7 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print()
         print('Interrupted \_[o_0]_/')
+        updateSettingsFile()
         try:
             sys.exit(0)
         except SystemExit:
